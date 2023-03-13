47ba112604044f2b761e9142b2038c4be0eda7b9
ach113 <archieberidze@gmail.com>
2023-03-13 13:25:05 -0700

    optimize `evaluate_model` function

--- src/eval.py
+++ src/eval.py
@@ -4,28 +4,31 @@ based on code taken from: https://github.com/hexiangnan/neural_collaborative_fil
 import math
 import heapq
 import numpy as np
+import tensorflow as tf
+from typing import Tuple, List
 
 # TODO!: Seems very slow, perhaps can be optimized
 
 
-def get_metrics(rank_list, item):
+def get_metrics(rank_list: List, item: int) -> Tuple[int, float]:
     if item not in rank_list:
         return 0, 0
     return 1, math.log(2) / math.log(rank_list.index(item) + 2)
 
 
-def evaluate_model(model, test_ratings, test_negatives, k: int, n_threads: int = 1):
+def evaluate_model(model: tf.keras.Model,
+                   users: List[int], items: List[int], negatives: List[List[int]],
+                   k: int, n_workers: int = 1) -> Tuple[float, float]:
     """
-    Evaluate the performance (Hit_Ratio, NDCG) of top-K recommendation
-    Return: score of each test rating.
+    calculates hit rate and normalized discounted cumulative gain for each user across each item in `negatives`
+    returns average of top-k list of hit rates and ndcgs
     """
     hits, ndcgs = list(), list()
-    users, items = zip(*test_ratings)
     for i, user in enumerate(users):
         item = items[i]
-        negatives = test_negatives[i] + [item]
+        negatives = negatives[i] + [item]
         pred = model.predict([np.full(len(negatives), user, dtype='int32'), np.array(negatives)],
-                             batch_size=100, verbose=0, workers=4)
+                             batch_size=100, verbose=0, workers=n_workers)
         map_item_score = dict(zip(negatives, pred))
         rank_list = heapq.nlargest(k, map_item_score, key=map_item_score.get)
         hr, ndcg = get_metrics(rank_list, item)
