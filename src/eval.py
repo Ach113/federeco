c721b3dcc911f2b6cc5a28764ef904634228f785
ach113 <archieberidze@gmail.com>
2023-03-13 20:14:08 -0700

    add docstring for `get_metrics`

--- src/eval.py
+++ src/eval.py
@@ -4,29 +4,38 @@ based on code taken from: https://github.com/hexiangnan/neural_collaborative_fil
 import math
 import heapq
 import numpy as np
+import tensorflow as tf
+from typing import Tuple, List
 
 # TODO!: Seems very slow, perhaps can be optimized
 
 
-def get_metrics(rank_list, item):
+def get_metrics(rank_list: List, item: int) -> Tuple[int, float]:
+    """
+    Used for calculating hit rate & normalized discounted cumulative gain (ndcg)
+    :param rank_list: Top-k list of recommendations
+    :param item: item we are trying to match with `rank_list`
+    :return: tuple containing 1/0 indicating hit/no hit & ndcg
+    """
     if item not in rank_list:
         return 0, 0
     return 1, math.log(2) / math.log(rank_list.index(item) + 2)
 
 
-def evaluate_model(model, test_ratings, test_negatives, k: int, n_threads: int = 1):
+def evaluate_model(model: tf.keras.Model,
+                   users: List[int], items: List[int], negatives: List[List[int]],
+                   k: int, n_workers: int = 1) -> Tuple[float, float]:
     """
-    Evaluate the performance (Hit_Ratio, NDCG) of top-K recommendation
-    Return: score of each test rating.
+    calculates hit rate and normalized discounted cumulative gain for each user across each item in `negatives`
+    returns average of top-k list of hit rates and ndcgs
     """
     hits, ndcgs = list(), list()
-    users, items = zip(*test_ratings)
     for i, user in enumerate(users):
         item = items[i]
-        negatives = test_negatives[i] + [item]
-        pred = model.predict([np.full(len(negatives), user, dtype='int32'), np.array(negatives)],
-                             batch_size=100, verbose=0, workers=4)
-        map_item_score = dict(zip(negatives, pred))
+        item_input = np.array(negatives[i] + [item])
+        user_input = np.full(len(item_input), user, dtype='int32')
+        pred = model.predict([user_input, item_input], batch_size=100, verbose=0, workers=n_workers)
+        map_item_score = dict(zip(item_input, pred))
         rank_list = heapq.nlargest(k, map_item_score, key=map_item_score.get)
         hr, ndcg = get_metrics(rank_list, item)
         hits.append(hr)
