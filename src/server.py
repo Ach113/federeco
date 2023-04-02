7dc9970d7461a17c714514f46053f283e4adb2e2
ach113 <archieberidze@gmail.com>
2023-04-02 16:24:07 -0700

    modify imports, allow passing of save path parameter to `run_server`

--- src/server.py
+++ src/server.py
@@ -1,50 +1,48 @@
-import os
+from model import NeuralCollaborativeFiltering as NCF
+from eval import evaluate_model
+from dataset import Dataset
+from config import DEVICE
+from client import Client
+from typing import List
+import collections
+import numpy as np
+import torch.nn
 import time
 import tqdm
 import copy
-import torch.nn
-import collections
-from typing import List
-
-from dataset import *
-from client import Client
-from eval import evaluate_model
-from model import NeuralCollaborativeFiltering as NCF
-
-from config import *
+import os
 
 
-def run_server(num_clients: int, num_rounds: int, save: bool) -> torch.nn.Module:
+def run_server(dataset: Dataset, num_clients: int, num_rounds: int, path: str) -> torch.nn.Module:
     """
     defines server side ncf model and initiates the training process
-    saves the trained model if appropriate parameter is set
+    saves the trained model at indicated path
     """
     # define server side model
-    server_model = NCF(NUM_USERS, NUM_ITEMS)
+    server_model = NCF(dataset.num_users, dataset.num_items)
     server_model.to(DEVICE)
 
     # if pretrained model already exists, loads its weights
     # if not, initiates the training process
-    trained_weights = torch.load(MODEL_SAVE_PATH) if os.path.exists(MODEL_SAVE_PATH) \
-        else training_process(server_model, num_clients, num_rounds)
+    trained_weights = torch.load(path) if os.path.exists(path) \
+        else training_process(dataset, server_model, num_clients, num_rounds)
 
-    if save:
-        torch.save(trained_weights, MODEL_SAVE_PATH)
+    torch.save(trained_weights, path)
     # load server model's weights to generate recommendations
     server_model.load_state_dict(trained_weights)
 
     return server_model
 
 
-def sample_clients(num_clients: int) -> List[Client]:
+def sample_clients(dataset: Dataset, num_clients: int) -> List[Client]:
     """
     initialize `num_clients` clients along with their respective data
     """
     clients = list()
     # load entire client dataset
-    client_dataset = load_client_train_data()
+    client_dataset = dataset.load_client_train_data()
     # sample random client ids
-    client_ids = np.random.choice(range(NUM_USERS), size=num_clients, replace=False)
+    client_ids = np.random.choice(range(dataset.num_users), size=num_clients, replace=False)
     for client_id in client_ids:
         c = Client(client_id)
         c.set_client_data(client_dataset[client_id])
@@ -53,7 +51,8 @@ def sample_clients(num_clients: int) -> List[Client]:
     return clients
 
 
-def training_process(server_model: torch.nn.Module,
+def training_process(dataset: Dataset,
+                     server_model: torch.nn.Module,
                      num_clients: int,
                      num_rounds: int) -> collections.OrderedDict:
     """
@@ -63,10 +62,10 @@ def training_process(server_model: torch.nn.Module,
         3. aggregates weights across `num_clients` clients and sets them to server model
     returns weights of a trained model
     """
-    test_data, negatives = load_test_file(), load_negative_file()
+    test_data, negatives = dataset.load_test_file(), dataset.load_negative_file()
 
     for _ in tqdm.tqdm(range(num_rounds)):
-        clients = sample_clients(num_clients)
+        clients = sample_clients(dataset, num_clients)
         w = single_train_round(server_model, clients)
         updated_server_weights = federated_averaging(w)
         server_model.load_state_dict(updated_server_weights)
