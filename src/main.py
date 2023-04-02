ec609481375dc2f059579ee80e6ae544437fc8a6
ach113 <archieberidze@gmail.com>
2023-04-02 16:24:30 -0700

    add argument parsing to support different datasets & save paths

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
 from server import run_server, sample_clients
+from dataset import Dataset
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog='federeco',
+        description='federated recommendation system',
+    )
+
+    parser.add_argument('-d', '--dataset', default='movielens', metavar='dataset',
+                        choices=['movielens', 'pinterest'],
+                        help='which dataset to use, default "movielens"')
+    parser.add_argument('-p', '--path', default='pretrained/ncf.h5', metavar='path',
+                        help='path where trained model is stored, default "pretrained/ncf.h5"')
+    return parser.parse_args()
 
 
 def main():
-    model = run_server(num_clients=20, num_rounds=400, save=True)
-    client = sample_clients(1)[0]
-    recommendations = client.generate_recommendation(model, k=10)
+    args = parse_arguments()
+    dataset = Dataset(args.dataset)
+    trained_model = run_server(dataset, num_clients=20, num_rounds=400, path=args.path)
+    client = sample_clients(dataset, 1)[0]
+    recommendations = client.generate_recommendation(server_model=trained_model, num_items=dataset.num_items, k=10)
     print(recommendations)
 
 
